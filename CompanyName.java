2f92f55968edd50e81844e8ba61ab4c0ec86184d
loganayaki1989 <129322073+loganayaki1989@users.noreply.github.com>
2023-04-04 19:52:19 +0530

    Add files via upload

---
+++ CompanyName.java
@@ -0,0 +1,27 @@
+package Week2.day2;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+
+public class CompanyName {
+	public static void main(String[] args)throws InterruptedException {
+		// TODO Auto-generated method stub
+		EdgeDriver driver = new EdgeDriver();
+		Thread.sleep(3000);
+	driver.get("http://leaftaps.com/opentaps/control/login");
+	driver.findElement(By.id("username")).sendKeys("Democsr");
+	driver.findElement(By.id("password")).sendKeys("crmsfa");
+	driver.findElement(By.className("Decosubmit")).click();
+    driver.findElement(By.linkText("CRM/SFA")).click();
+	driver.findElement(By.linkText("leads")).click();
+	driver.findElement(By.id("Create Lead")).click();
+	driver.findElement(By.id("Companyname")).sendKeys("TestLeaf.org");
+	driver.findElement(By.id("Firstname")).sendKeys("Sai");
+	driver.findElement(By.id("Lastname")).sendKeys("Sharvesh.R");
+	driver.findElement(By.id("Emai-id")).sendKeys("loganayaki.msc@gmail.com");
+    driver.findElement(By.id("Phoneno")).sendKeys("8939797330");
+	driver.findElement(By.name("submitbutton")).click();
+    }
+	}
+	
---
+++ UpdateChecking.java
@@ -0,0 +1,33 @@
+package Week2.day2;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+
+public class UpdateChecking {
+
+	public static void main(String[] args)throws InterruptedException {
+		EdgeDriver driver = new EdgeDriver();
+		Thread.sleep(3000);
+	driver.get("http://leaftaps.com/opentaps/control/login");
+	driver.findElement(By.id("username")).sendKeys("Democsr");
+	driver.findElement(By.id("password")).sendKeys("crmsfa");
+	driver.findElement(By.id("login")).click();
+    driver.findElement(By.className("Decosubmit")).click();
+    driver.findElement(By.linkText("CRM/SFA")).click();
+	driver.findElement(By.linkText("leads")).click();
+	driver.findElement(By.id("Create Lead")).click();
+	driver.findElement(By.id("Companyname")).sendKeys("TestLeaf.org");
+	driver.findElement(By.id("Firstname")).sendKeys("Sai");
+	driver.findElement(By.id("Lastname")).sendKeys("Sharvesh.R");
+	driver.findElement(By.name("submitbutton")).click();
+    driver.findElement(By.linkText("Edit")).click();
+	WebElement Update=driver.findElement(By.id("companyName updation"));
+	Update.sendKeys("Org.TestLeaf");
+	driver.findElement(By.name("submitButton")).click();
+	
+	driver.close();
+	}
+}
+
+
